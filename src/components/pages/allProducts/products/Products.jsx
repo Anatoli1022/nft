6035d2d7d767549eaf85ab4238bb8e978d9b0810
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-05 16:49:52 +0300

    feat: adds page

---
+++ src/components/pages/allProducts/products/Products.jsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from './Products.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Products = () => {
+  return <div className={cx('')}></div>;
+};
+
+export default Products;
