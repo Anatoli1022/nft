d2af3b00d09aa49314ea07289bb1f8a276222b07
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-05 16:50:14 +0300

    feat: adds pages allProducts and login

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,11 @@
+import Products from '../components/pages/allProducts/products/Products';
+
+const AllProducts = () => {
+  return (
+    <>
+      <Products />
+    </>
+  );
+};
+
+export default AllProducts;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/pages/login/login-form/LoginForm';
+
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
