2625bd27476781bbf46883446e2a6c2035937651
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-05 16:48:46 +0300

    feat: adds new route in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/layout';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import AllProducts from './pages/AllProducts';
+import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="products" element={<AllProducts />} />
+          <Route path="login" element={<Login />} />
           <Route path="notFound" element={<NotFound />} />
         </Route>
       </Routes>
