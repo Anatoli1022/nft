677d8c5fc57437a7fa46d50754ddcaf59574dafb
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-16 20:56:50 +0300

    feat: adds new component in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,23 @@
-import LoginForm from '../components/pages/login/login-form/LoginForm';
+import { useState } from 'react';
+
+import FormIssuer from '../components/pages/login/formIssuer/FormIssuer';
+import FormVerifier from '../components/pages/login/formVerifier/FormVerifier';
+import FormConsumer from '../components/pages/login/formConsumer/FormConsumer';
+import ButtonsList from '../components/pages/login/buttonsList/ButtonsList';
 
 const Login = () => {
+  const [showForm, setShowForm] = useState('issuer');
+
+  const toggleForm = (form) => {
+    setShowForm(form);
+  };
   return (
-    <>
-      <LoginForm />
-    </>
+    <div className={'container'}>
+      <ButtonsList toggleForm={toggleForm} showForm={showForm} />
+      {showForm === 'issuer' ? <FormIssuer /> : null}
+      {showForm === 'verifier' ? <FormVerifier /> : null}
+      {showForm === 'consumer' ? <FormConsumer /> : null}
+    </div>
   );
 };
 
