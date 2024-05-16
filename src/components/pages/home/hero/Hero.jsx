2f72b0773038a5dbdc15cbbcc225ba63480ca1e6
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-16 21:18:41 +0300

    fix: fix component hero

--- src/components/pages/home/hero/Hero.jsx
+++ src/components/pages/home/hero/Hero.jsx
@@ -2,21 +2,11 @@ import classNames from 'classnames/bind';
 
 import styles from './Hero.module.scss';
 
-import background from './images/background.jpg';
-
 const cx = classNames.bind(styles);
 const Hero = () => {
   return (
     <section className={cx('hero')}>
-      <div className={cx('container', 'hero-container')}>
-        <img
-          src={background}
-          alt=""
-          aria-hidden="true"
-          className={cx('background')}
-          loading="eager"
-        />
-      </div>
+      <div className={cx('container', 'hero-container')}></div>
     </section>
   );
 };
